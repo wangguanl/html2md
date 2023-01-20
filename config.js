be8da87b6851181f61060e6b29aece76c1a9b8cd
wanggang <wanggang@dyrs.com.cn>
2023-01-20 11:34:30 +0800

    1

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  url: 'http://localhost:8080/index.html',
-  name: 'xxx',
+  url: 'https://juejin.cn/post/7183705937180524601',
+  name: 'index',
   hosts: {
     'www.cnblogs.com': '#cnblogs_post_body',
     'www.bilibili.com': '.article-container__content',
-    localhost: '.article-container__content',
+    'juejin.cn': '.markdown-body',
+    // localhost: '.article-container__content',
   },
 };
